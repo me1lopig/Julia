9073e54cafd8887d72d2ee197175e9230cd4d063
me1lopig <me1lopig@uco.es>
2022-06-12 13:01:04 +0200

    20220612

---
+++ bucle.jl
@@ -0,0 +1,7 @@
+# ejemplo de bucle
+# con numeros aleatorios
+
+for i in 1:10
+    x = rand(1:20)
+    println(i," ",x)
+  end
--- ejemplo.jl
+++ ejemplo.jl
@@ -1,3 +1,5 @@
+# ejemplo sencillo de Programa en Julia
+
 println("Hola mundo desde Julia")
 println("Primeros pasos en Julia")
 print("Comenzamos en MacOs")
